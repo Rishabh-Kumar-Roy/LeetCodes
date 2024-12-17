9c5d075ce0559ca291428a0da3baf66bf0bf4d3f
Rishabh Kumar Roy <92028533+Rishabh-Kumar-Roy@users.noreply.github.com>
2024-12-17 19:03:37 +0530

    Time: 10 ms (11.29%), Space: 44.6 MB (45.3%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -1,21 +1,18 @@
 class Solution {
     public boolean judgeCircle(String moves) {
-        int u=0,d=0,l=0,r=0;
+        int u=0,l=0;
         int len=moves.length();
         for(int i=0;i<len;i++)
         {
             if(moves.charAt(i)=='U')
             u++;
             if(moves.charAt(i)=='D')
-            d++;
+            u--;
             if(moves.charAt(i)=='L')
             l++;
             if(moves.charAt(i)=='R')
-            r++;
+            l--;
         }
-        if(l-r==0 && u-d==0)
-        return true;
-
-        return false;
+        return u==0&&l==0;
     }
 }
