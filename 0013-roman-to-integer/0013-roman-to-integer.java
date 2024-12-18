0b41ea67b1d642e03bce50571cee9c5507919133
Rishabh Kumar Roy <92028533+Rishabh-Kumar-Roy@users.noreply.github.com>
2024-12-18 13:35:15 +0530

    Time: 2 ms (100%), Space: 44.4 MB (88.57%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,43 +1,39 @@
 class Solution {
     public int romanToInt(String s) {
-        int n=s.length();
-        int num=0,prev=0,ans=0;
-        for(int i=n-1;i>=0;i--)
+        int l=s.length();
+        int num=0,ans=0,prev=0;
+        for(int i=l-1;i>=0;i--)
         {
-            char a=s.charAt(i);
-            switch(a)
+            switch(s.charAt(i))
             {
                 case 'I':
                 num=1;
                 break;
-                 case 'V':
-                 num=5;
+                case 'V':
+                num=5;
                 break;
-                 case 'X':
-                 num=10;
+                case 'X':
+                num=10;
                 break;
-                 case 'L':
-                 num=50;
+                case 'L':
+                num=50;
                 break;
-                 case 'C':
-                 num=100;
+                case 'C':
+                num=100;
                 break;
-                 case 'D':
-                 num=500;
+                case 'D':
+                num=500;
                 break;
-                 case 'M':
-                 num=1000;
+                case 'M':
+                num=1000;
                 break;
             }
-                if(num<prev)
-                ans-=num;
-                else
-                ans+=num;
-                prev=num;
-                
-            
+            if(num<prev)
+            ans-=num;
+            else
+            ans+=num;
+            prev=num;
         }
         return ans;
-        
     }
 }
