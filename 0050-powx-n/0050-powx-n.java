66324180d30d7539b9fa5e0f2c4664c2c900e90c
Rishabh Kumar Roy <92028533+Rishabh-Kumar-Roy@users.noreply.github.com>
2025-01-06 10:11:00 +0530

    Time: 0 ms (100%), Space: 43.2 MB (13.74%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,13 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n==0)
+        return 1;
+        double a=myPow(x,n/2);
+        if(n%2==0)
+        return a*a;
+        if(n>0)
+        return a*a*x;
+        return a*a/x;
+        
+    }
+}
