c5c872e412fad7b8c9d815b56e5b049fa422d2e9
Rishabh Kumar Roy <92028533+Rishabh-Kumar-Roy@users.noreply.github.com>
2025-02-02 23:14:17 +0530

    Time: 4 ms (26.68%), Space: 46.4 MB (67.56%) - LeetHub

---
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -0,0 +1,33 @@
+class Solution {
+        public int longestOnes(int[] A, int K) {
+        
+        int left = 0,   // start of the window
+        max = 0,        // len of max valid window
+        zeros = 0;      // number of zeros in a window 
+        
+        if(A == null || A.length == 0){
+            return 0;
+        }
+        
+        for(int i = 0 ; i < A.length ; i++){
+            
+            // Increase count of zero whenever you see one
+            if(A[i]==0){
+                zeros++;
+            }
+            
+            // Shrink the window until zeros are equal less than required k
+            while(left <= i && zeros > K){
+                if(A[left] == 0){
+                    zeros--;
+                }   
+                left++;
+            }
+            
+            // At every step record the length of the window
+            max = Math.max(max, i - left +1);
+        } 
+        
+        return max;
+    }
+}
