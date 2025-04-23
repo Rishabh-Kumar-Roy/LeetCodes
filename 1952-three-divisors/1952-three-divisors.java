6b4a70c1cd32bec9cf1032726da591c85bcd08b5
Rishabh Kumar Roy <92028533+Rishabh-Kumar-Roy@users.noreply.github.com>
2025-04-23 10:56:50 +0530

    Time: 0 ms (100%), Space: 40.4 MB (84.7%) - LeetHub

--- 1952-three-divisors/1952-three-divisors.java
+++ 1952-three-divisors/1952-three-divisors.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isThree(int n) {
-        int c=0;
-        for(int i=1;i<=n;i++)
+        int c=2;
+        for(int i=2;i<=n/2;i++)
         {
             if(n%i==0)
             c++;
