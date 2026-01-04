fdf038db4735ca58ab32f7e3d8482a8bb7b9b3b2
Rishabh Kumar Roy <92028533+Rishabh-Kumar-Roy@users.noreply.github.com>
2026-01-04 11:22:11 +0530

    Time: 15 ms (42%), Space: 17.6 MB (83.05%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,19 +1,21 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        ck = {}
-        ck1 = {}
-
+        ck={}
+        ck1={}
         for i in s:
             if i in ck:
-                ck[i] += 1
+                ck[i]+=1
             else:
-                ck[i] = 1
-
+                ck[i]=1
         for j in t:
             if j in ck1:
-                ck1[j] += 1
+                ck1[j]+=1
             else:
-                ck1[j] = 1
+                ck1[j]=1
+        if (ck==ck1):
+            return True
+        return False
+        
+        
 
-        return ck == ck1
         
