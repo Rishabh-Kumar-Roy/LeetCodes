d618a403af340ccb0e3faad5ba1a751279002c9a
Rishabh Kumar Roy <92028533+Rishabh-Kumar-Roy@users.noreply.github.com>
2026-08-18 10:20:46 +0530

    Time: 15 ms (40.58%), Space: 22 MB (64.11%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        group={}
+        for word in strs:
+            sorted_word=sorted(word)
+            key=''.join(sorted_word)
+            if key not in group:
+                group[key]=[]
+            group[key].append(word)
+        return list(group.values())
+
+
+        
